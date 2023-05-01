73fcf62c81646c1ff5f21ecc18bbd94a0ab9ffba
Filipe <silvafilipeuk@gmail.com>
2023-05-01 11:35:04 +0100

    Initial files. Dataset.

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+use f1
+
+go
+
+
+-- select * from drivers
+
+-- select count(*) as 'No_drivers' from drivers
+
+--SELECT nationality, count(nationality) as 'No_of_drivers_per_country'
+--FROM drivers d
+--GROUP BY nationality
